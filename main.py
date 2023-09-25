313cab73f698e11764828697628cf6123ce61465
GolubevDA <999gothicman999@mail.ru>
2023-09-25 15:29:52 +0700

    First project on Flask

--- main.py
+++ main.py
@@ -3,17 +3,17 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template('index.html')
 
 @app.route('/parsing.html')
 def parsing():
-    return 'Pasring vacancy python'
+    return render_template('parsing.html')
 
 @app.route('/resume.html')
 def resume():
-    return 'Python the best language programming'
+    return render_template('resume.html')
 
 
 if __name__ == '__main__':
