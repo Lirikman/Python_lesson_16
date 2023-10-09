b2530c5db6ffc07046c54e87b260fe8e0e7ed36b
GolubevDA <999gothicman999@mail.ru>
2023-10-09 16:18:16 +0700

    First project on Flask

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 @app.route('/index.html')
@@ -12,9 +13,18 @@ def index():
 def parsing():
     return render_template('parsing.html')
 
+
 @app.route('/resume.html')
 def resume():
     return render_template('resume.html')
+
+
+@app.route('/result.html', methods=['POST'])
+def result():
+    city = request.form['city']
+    vac = request.form['vac']
+    print(city, vac)
+    return render_template('result.html')
 
 
 if __name__ == '__main__':
